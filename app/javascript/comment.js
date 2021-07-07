28f2766793bbdb2eae77c2da2c1567c76a9fe883
shunsaku0621 <hamagi.0621@icloud.com>
2021-07-07 09:24:14 +0900

    修正

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,14 +1,27 @@
+// function post (){
+//   const submit = document.getElementById("post-comment-submit");
+//   submit.addEventListener("click", (e)=>{
+//     e.preventDefault();
+//     const form = document.getElementById("comment-form");
+//     const formData = new FormData(form);
+//     const XHR = new XMLHttpRequest();
+//     XHR.open("POST", `/posts/gon.comment./comments`, true);
+//     XHR.responseType = "json";
+//     XHR.send(formData);
+//   });
+// };
+
+// window.addEventListener('load', post);
+
 function post (){
   const submit = document.getElementById("post-comment-submit");
-  submit.addEventListener("click", ()=>{
-    // e.preventDefault();
-    const form = document.getElementById("comment-form");
-    const formData = new FormData(form);
-    const XHR = new XMLHttpRequest();
-    XHR.open("POST", `/posts/2/comments`, true);
-    XHR.responseType = "json";
-    XHR.send(formData);
+  submit.addEventListener("click", (e)=>{
+    
+    window.alert(`hello,${gon.comment.id}!`);  
+    debugger;
+    // console.log(gon.comment);
   });
 };
 
 window.addEventListener('load', post);
+
